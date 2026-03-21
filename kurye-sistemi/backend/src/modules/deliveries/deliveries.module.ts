0e60ddef054d8a6bd2e6aa5dec80a5442b5ae596
Paketci Admin <admin@paketci.com>
2026-03-21 18:29:12 +0800

    fix: DeliveriesModule service and exports added

--- kurye-sistemi/backend/src/modules/deliveries/deliveries.module.ts
+++ kurye-sistemi/backend/src/modules/deliveries/deliveries.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeliveriesController } from './deliveries.controller';
+import { DeliveriesService } from './deliveries.service';
 import { Delivery } from './entities/delivery.entity';
+import { Company } from '../companies/entities/company.entity';
+import { Credit } from '../credits/entities/credit.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Delivery])],
+  imports: [TypeOrmModule.forFeature([Delivery, Company, Credit])],
   controllers: [DeliveriesController],
+  providers: [DeliveriesService],
+  exports: [DeliveriesService],
 })
 export class DeliveriesModule {}
