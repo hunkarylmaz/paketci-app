b5dbb352b57641e6320cb1c8527c9765387edc6f
Paketci Admin <admin@paketci.com>
2026-03-21 18:23:05 +0800

    fix: IntegrationsModule HttpModule added

--- kurye-sistemi/backend/src/modules/integrations/integrations.module.ts
+++ kurye-sistemi/backend/src/modules/integrations/integrations.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { IntegrationsController } from './integrations.controller';
 import { IntegrationsService } from './integrations.service';
 import { RestaurantPlatformConfig } from './entities/restaurant-platform-config.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RestaurantPlatformConfig])],
+  imports: [TypeOrmModule.forFeature([RestaurantPlatformConfig]), HttpModule],
   controllers: [IntegrationsController],
   providers: [IntegrationsService],
 })
