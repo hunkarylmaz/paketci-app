23659431587165ec9e7ba3e94ac659d206f0367b
Paketci Admin <admin@paketci.com>
2026-03-21 18:26:22 +0800

    fix: ReportsModule entities added

--- kurye-sistemi/backend/src/modules/reports/reports.module.ts
+++ kurye-sistemi/backend/src/modules/reports/reports.module.ts
@@ -3,11 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 import { Delivery } from '../deliveries/entities/delivery.entity';
+import { Courier } from '../couriers/entities/courier.entity';
+import { Restaurant } from '../restaurants/entities/restaurant.entity';
+import { User } from '../users/entities/user.entity';
+import { Company } from '../companies/entities/company.entity';
 import { CommonModule } from '../../common/common.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Delivery]),
+    TypeOrmModule.forFeature([Delivery, Courier, Restaurant, User, Company]),
     CommonModule,
   ],
   controllers: [ReportsController],
